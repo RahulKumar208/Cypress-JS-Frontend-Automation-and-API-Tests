a3f1a5a6bad3df0d5eced1992a46fa550b8a1907
Rahul kumar <rahulkumar208>
2024-08-31 15:00:39 +0200

    Initial commit to QA branch

---
+++ cypress/e2e/automation.cy.js
@@ -0,0 +1,8 @@
+// cypress/integration/insurance_xpath_spec.js
+
+describe('Frontend Test', () => {
+  it('test1', () => {
+    cy.visit('https://www.ottonova.de/online-beitragsrechner');
+   // cy.xpath("//button[contains(text(), 'Alles Akzeptieren')]").click({ force: true });
+  });
+});
