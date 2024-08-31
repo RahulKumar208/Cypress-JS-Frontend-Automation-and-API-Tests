81cd6572462dcde1ffcd7fa49d3a8087ed539571
Rahul kumar <rahulkumar208>
2024-08-31 15:46:55 +0200

    Fontend Test - Visit URL and Select employment and input value

--- cypress/e2e/automation.cy.js
+++ cypress/e2e/automation.cy.js
@@ -1,8 +1,22 @@
 // cypress/integration/insurance_xpath_spec.js
 
-describe('Frontend Test', () => {
-  it('test1', () => {
+describe('Insurance Premium Calculator Using XPath', () => {
+  
+  beforeEach(() => {
+    // Navigate to the insurance calculator page
     cy.visit('https://www.ottonova.de/online-beitragsrechner');
-   // cy.xpath("//button[contains(text(), 'Alles Akzeptieren')]").click({ force: true });
+
+    // Check if the "Alles Akzeptieren" button for cookies is present
+   
   });
+
+  it('Selects employment type and inputs income', () => {
+    // Select "Angestellt" employment type
+    cy.xpath("//*[@data-cy='employment-status-option-employed']").click();
+    // Enter an income of "70000"
+    cy.xpath("//*[@data-cy='income-input' and @placeholder='Bitte hier eingeben']").type('70000');
+    cy.xpath("//button[@type='submit' and @data-cy='employment-status-continue']").click();
+  });
+
+  
 });
